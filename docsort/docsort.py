b3a0746244972f931d059e2a91fd0b3c01ca979d
neumann <neumann@192.168.2.185>
2024-03-28 22:21:49 +0100

    create repository

---
+++ docsort/docsort.py
@@ -0,0 +1,17 @@
+'''
+Created on 28.03.2024
+
+@author: neumann
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
