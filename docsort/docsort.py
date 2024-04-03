adfbf86d82d4da86e1a72e8b3136f9c4ddb21a0a
Holger Döring <doering@uni-bremen.de>
2024-04-03 23:18:32 +0200

    Reformat and lint code with Ruff

--- docsort/docsort.py
+++ docsort/docsort.py
@@ -1,15 +1,15 @@
-'''
+"""
 Created on 02.04.2024
 
 @author: neumann
-'''
+"""
+
 
 class DocSort(object):
-
     def __init__(self, scanPath, docPath):
         self.scanPath = scanPath
-        self.docPath  = docPath
-    
+        self.docPath = docPath
+
     def getDocs(self):
         return 0
 
@@ -18,10 +18,7 @@ class DocSort(object):
 
     def convertDoc(self, idx):
         return 0
-    
+
 
 if __name__ == "__main__":
-    docs = DocSort("scans", "docs" )
-
-
-
+    docs = DocSort("scans", "docs")
--- docsort/temp.py
+++ docsort/temp.py
@@ -1,18 +1,17 @@
-'''
+"""
 Created on 02.04.2024
 
 @author: neumann
-'''
+"""
 import fitz
 import easyocr
 import os
-import shutil
 import glob
 
-scanspath = 'scans'
-docspath = 'docs'
+scanspath = "scans"
+docspath = "docs"
 
-companies= ["Pydev", "Verein", "Montag", "docsort"]
+companies = ["Pydev", "Verein", "Montag", "docsort"]
 pdffiles = glob.glob(os.path.join(scanspath, "*.pdf"))
 reader = easyocr.Reader(["de", "en"])
 
@@ -23,14 +22,20 @@ for pdfpathname in pdffiles:
     page = doc.load_page(0)
     page.clean_contents()
     print(len(page.get_text()))
-    if len(page.get_text())<100: 
+    if len(page.get_text()) < 100:
         pixmap = page.get_pixmap(dpi=288)
         img = pixmap.tobytes()
         result = reader.readtext(img)
-    
+
         pathname = ""
-        for p, text,x  in result:
-            page.insert_text((p[3][0]/4, p[3][1]/4), text, fontsize=10, stroke_opacity=0.0, fill_opacity=0.0)
+        for p, text, x in result:
+            page.insert_text(
+                (p[3][0] / 4, p[3][1] / 4),
+                text,
+                fontsize=10,
+                stroke_opacity=0.0,
+                fill_opacity=0.0,
+            )
             if text in companies and not pathname:
                 if not os.path.isdir(os.path.join(docspath, text)):
                     os.makedirs(os.path.join(docspath, text), exist_ok=True)
@@ -39,22 +44,6 @@ for pdfpathname in pdffiles:
     else:
         if page.get_text() in companies:
             print(f"copy '{pdfname}'")
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 """
@@ -65,4 +54,3 @@ for i, info in enumerate(doc.pages()):
     for _, text, _ in result:
         print(text)
 """
-
